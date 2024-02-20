0c29ddc98177614ab0f73770cc87863fe2f04928
Nekoma <147964939+QNekoma@users.noreply.github.com>
2024-02-20 16:49:47 +0500

    Update 03_search.py

--- 03_search.py
+++ 03_search.py
@@ -1,12 +1,11 @@
-import math
-
-for num in range(174457, 174505 + 1):
+for i in range(174457, 174506):
     divisors = []
-    for i in range(2, int(math.sqrt(num)) + 1):
-        if num % i == 0:
-            divisors.append(i)
-            if i != num // i:
-                divisors.append(num // i)
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            if j not in divisors:
+                divisors.append(j)
+            if i // j not in divisors:
+                divisors.append(i // j)
+    divisors.sort()
     if len(divisors) == 2:
-        divisors.sort()
         print(divisors[0], divisors[1])
