a7f40a97cdcd4ff77334b379b35277ea6af2cb13
Nekoma <147964939+QNekoma@users.noreply.github.com>
2024-02-20 17:53:46 +0500

    Update 01_name.py

--- 01_name.py
+++ 01_name.py
@@ -1,13 +1,11 @@
+from itertools import product
+
 letters = ['Т', 'И', 'М', 'О', 'Ф', 'Е', 'Й']
 valid_codes = 0
 
-for letter1 in letters:
-    for letter2 in letters:
-        for letter3 in letters:
-            for letter4 in letters:
-                for letter5 in letters:
-                    code = letter1 + letter2 + letter3 + letter4 + letter5
-                    if code.count('Й') <= 1 and 'ИЙ' not in code and code[1] != 'Й' and code[-1] != 'Й':
-                        valid_codes += 1
+for code in product(letters, repeat=5):
+    if code.count('Й') <= 1 and 'ИЙ' not in code and code[1] != 'Й' and code[-1] != 'Й':
+        valid_codes += 1
 
 print(valid_codes)
+
